91d1b10c3a65a013411e74069c8399d59f792981
Olivier <olivier.ledc@gmail.com>
2023-06-08 14:26:33 +0200

    hapi server works

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import Hapi from '@hapi/hapi'
+
+const start = async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost',
+    })
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: (req, h) => {
+            return 'Hello!';
+        }
+    })
+
+    await server.start();
+    console.log(`server is listening on ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', error => {
+    console.error(error);
+    process.exit(1);
+});
+
+start();
