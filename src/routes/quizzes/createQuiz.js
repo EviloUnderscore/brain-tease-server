83e30d3370c255afc823a15e97da67a2171701aa
Olivier <olivier.ledc@gmail.com>
2023-06-13 12:25:00 +0200

    add token

--- src/routes/quizzes/createQuiz.js
+++ src/routes/quizzes/createQuiz.js
@@ -1,5 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import { db } from '../../database';
+import * as admin from 'firebase-admin';
 
 export const createQuizRoute = {
     method: 'POST',
@@ -7,12 +8,17 @@ export const createQuizRoute = {
     handler: async (req, h) => {
         const id = uuid();
         const { name = '', description = '', category_id = ''} = req.payload;
+        
+        const token = req.headers.authtoken;
+        const user = await admin.auth().verifyIdToken(token);
+        const user_id = user.user_id;
+       
         await db.query(`
-                INSERT INTO quizzes (id, name, description, category_id)
-                VALUES (?, ?, ?, ?)
+                INSERT INTO quizzes (id, name, description, category_id, user_id)
+                VALUES (?, ?, ?, ?, ?)
             `,
-            [id, name, description, category_id]
+            [id, name, description, category_id, user_id]
         );
-        return { id, name, description, category_id}
+        return { id, name, description, category_id, user_id}
     }
 }
