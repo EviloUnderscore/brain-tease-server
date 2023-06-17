61f674262cbf2a6fde402962f096752f9e2c5d63
Olivier Leduc <olivier.ledc@gmail.com>
2023-06-17 22:19:45 +0200

    ss

--- src/routes/questions/deleteByQuizId.js
+++ src/routes/questions/deleteByQuizId.js
@@ -4,6 +4,7 @@ export const deleteByQuizIdRoute = {
     method: 'DELETE',
     path: '/api/questions/{id}',
     handler: async (req, h) => {
+        console.log(44444444444);
         const { id } = req.params;
         await db.query(
             'DELETE FROM questions WHERE quiz_id=?',
