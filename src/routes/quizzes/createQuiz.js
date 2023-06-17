b0108e7d7a3a3a0087c993e6ad9d6a30a33774be
Olivier Leduc <olivier.ledc@gmail.com>
2023-06-17 14:49:40 +0200

    add author name

--- src/routes/quizzes/createQuiz.js
+++ src/routes/quizzes/createQuiz.js
@@ -12,13 +12,16 @@ export const createQuizRoute = {
         const token = req.headers.authtoken;
         const user = await admin.auth().verifyIdToken(token);
         const user_id = user.user_id;
+        const authorName = user.name;
+        console.log(333333333333333333);
+        console.log(user);
        
         await db.query(`
-                INSERT INTO quizzes (id, name, description, category_id, user_id)
-                VALUES (?, ?, ?, ?, ?)
+                INSERT INTO quizzes (id, name, description, category_id, user_id, authorName)
+                VALUES (?, ?, ?, ?, ?, ?)
             `,
-            [id, name, description, category_id, user_id]
+            [id, name, description, category_id, user_id, authorName]
         );
-        return { id, name, description, category_id, user_id}
+        return { id, name, description, category_id, user_id, authorName}
     }
 }
