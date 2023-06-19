ed79fe2305f8ace40e2b150832b42ee0769b73ad
Olivier Leduc <olivier.ledc@gmail.com>
2023-06-19 14:42:18 +0200

    rrr

--- src/routes/histories/createHistory.js
+++ src/routes/histories/createHistory.js
@@ -1,5 +1,6 @@
 import { v4 as uuid } from 'uuid';
 import { db } from '../../database';
+import * as admin from 'firebase-admin';
 
 export const createHistoryRoute = {
     method: 'POST',
@@ -9,15 +10,16 @@ export const createHistoryRoute = {
         const token = req.headers.authtoken;
         const user = await admin.auth().verifyIdToken(token);
         const user_id = user.user_id;
+        const createDate = new Date();
         const { quiz_id = '', 
                 score = ''} = req.payload;
 
         await db.query(`
-                INSERT INTO histories (id, score, user_id quiz_id)
-                VALUES (?, ?, ?, ?)
+                INSERT INTO histories (id, score, user_id, quiz_id, created_at)
+                VALUES (?, ?, ?, ?, ?)
             `,
-            [id, score, user_id, quiz_id]
+            [id, score, user_id, quiz_id, createDate]
         );
-        return {id, score, user_id, quiz_id}
+        return {id, score, user_id, quiz_id, createDate}
     }
 }
